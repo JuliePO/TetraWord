3bff856c9e88eeff0371dc3b6321d9b680885855
NicolasB <nicolasj.bertrand@gmail.com>
2014-05-10 17:14:16 +0200

    correction beug

--- TetraWord/tw/src/Graphic/option/option/OptionUnderPanel.java
+++ TetraWord/tw/src/Graphic/option/option/OptionUnderPanel.java
@@ -6,46 +6,60 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import utility.Configuration;
 import utility.Player;
+import Graphic.ButtonTetra;
 import Graphic.PanelBase;
 import Graphic.option.TetraComboBox;
 import Graphic.tetra.BonusComponent;
 
 public class OptionUnderPanel extends PanelBase {
+	
+	private Configuration config;
+	private NameComponent name1;
+	private NameComponent name2;
+	private TetraComboBox<String> speed;
+	private TetraComboBox<String> rateBonus;
+	private TetraComboBox<String> dico;
+	private JoypadComponent inputPlayer1;
+	private JoypadComponent inputPlayer2;
 
-	public OptionUnderPanel(Player p1, Player p2) {
+	public OptionUnderPanel(Player p1, Player p2, Configuration config) {
 		state = 'o';
 		
 		w = 1024;
 		h = 700;
 		
+		this.config=config;
+		
 		setLayout(null);
 		
-		NameComponent name1 = new NameComponent(p1, "Player 1");
+		name1 = new NameComponent(p1, "Player 1");
 		name1.setBounds(70, 113, name1.getW(), name1.getH());
 		add(name1);
 		
-		NameComponent name2 = new NameComponent(p2, "Player 2");
+		name2 = new NameComponent(p2, "Player 2");
 		name2.setBounds(607, 113, name2.getW(), name2.getH());
 		add(name2);
 		
 		String[] array = {"Slow", "Normal", "Fast"};
-		TetraComboBox<String> speed = new TetraComboBox<>("Speed", array, 91, 263);
+		speed = new TetraComboBox<>("Speed", array, 91, 263);
 		speed.setBounds(70, 187, speed.getWidth(), speed.getHeight());
 		add(speed);
 		
-		TetraComboBox<String> rateBonus = new TetraComboBox<>("Rate bonus", array, 127, 263);
+		rateBonus = new TetraComboBox<>("Rate bonus", array, 127, 263);
 		rateBonus.setBounds(570, 187, rateBonus.getWidth(), rateBonus.getHeight());
 		add(rateBonus);
 		
-		String[] dicoArray = {"Fançais"};
-		TetraComboBox<String> dico = new TetraComboBox<>("Dictionnary", dicoArray, 140, 263);
+		dico = new TetraComboBox<>("Dictionnary", config.lang, 140, 263);
 		dico.setBounds(70, 261, dico.getWidth(), dico.getHeight());
 		add(dico);
 		
@@ -55,14 +69,45 @@ public class OptionUnderPanel extends PanelBase {
 		joypad.setBounds(70, 327, 175, 42);
 		add(joypad);
 		
-		JoypadComponent inputPlayer1 = new JoypadComponent("Player 1", p1);
+		inputPlayer1 = new JoypadComponent("Player 1", p1);
 		inputPlayer1.setBounds(0, 398, inputPlayer1.getW(), inputPlayer1.getH());
 		add(inputPlayer1);
 		
-		JoypadComponent inputPlayer2 = new JoypadComponent("Player 2", p2);
+		inputPlayer2 = new JoypadComponent("Player 2", p2);
 		inputPlayer2.setBounds(511, 398, inputPlayer2.getW(), inputPlayer2.getH());
 		add(inputPlayer2);
 		
+		String path = mPath + "texture/options/buttons/";
+		
+		ButtonTetra letter = new ButtonTetra(path + "letters.png", path +"letters_hover.png");
+		letter.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				state = 'l';
+			}
+		});
+		letter.setBounds(71, 610, letter.getWidthTexture(), letter.getHeightTexture());
+		add(letter);
+		
+		ButtonTetra shapes = new ButtonTetra(path + "shapes.png", path +"shapes_hover.png");
+		shapes.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				state = 's';
+			}
+		});
+		shapes.setBounds(416, 610, shapes.getWidthTexture(), shapes.getHeightTexture());
+		add(shapes);
+		
+		ButtonTetra exit = new ButtonTetra(path + "return.png", path +"return_hover.png");
+		exit.addActionListener(new ActionListener() {			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				state = 'r';
+			}
+		});
+		exit.setBounds(760, 610, exit.getWidthTexture(), exit.getHeightTexture());
+		add(exit);
 	}
 	
 	@Override
@@ -78,15 +123,33 @@ public class OptionUnderPanel extends PanelBase {
 	
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
+		name1.update();
+		name2.update();
+		inputPlayer1.update();
+		inputPlayer2.update();
 
+
+		if(speed.select){
+			config.setSpeedGame(speed.getSelectItem());
+			speed.select = false;
+		}
+		if(rateBonus.select){
+			config.setRateBonus(rateBonus.getSelectItem());
+			rateBonus.select = false;
+		}
+		if(dico.select){
+			config.setLang(dico.getSelectItem());
+			dico.select = false;
+		}
+		
+		repaint();
 	}
 	
 	public static void main(String[] args) {
 		JFrame tmp = new JFrame();
 		
 		tmp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		OptionUnderPanel bonus = new OptionUnderPanel(new Player(1, "georges"), new Player(2, "Louis"));	
+		OptionUnderPanel bonus = new OptionUnderPanel(new Player(1, "georges"), new Player(2, "Louis"), new Configuration());	
 		tmp.setContentPane(bonus);
 		tmp.pack();
 		tmp.setVisible(true);
